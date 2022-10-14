e52ea83d4ed91e97f3df05e4f5f6db9ebf2d3137
Slava <svyatoslav.rudakas@yandex.ru>
2022-10-14 09:24:16 +0300

    correct exceptions

--- src/main/java/ru/yandex/practicum/filmorate/controller/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/HandlerException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.FilmUserNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
@@ -13,13 +11,13 @@ public class HandlerException {
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e){
-        return new ErrorResponse(String.format(e.getMessage()));
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(FilmUserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException (final FilmUserNotFoundException e){
-        return new ErrorResponse(String.format(e.getMessage()));
+        return new ErrorResponse(e.getMessage());
     }
 
    @ExceptionHandler(Throwable.class)
